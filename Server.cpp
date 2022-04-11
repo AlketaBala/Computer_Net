c1f6fa724b913a4e4eaaaba3c54a40c187ea89df
AlbinHashani <94253491+AlbinHashani@users.noreply.github.com>
2022-04-11 23:50:17 +0200

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -15,3 +15,42 @@ void main()
 	{
 		cout << "Can't start the Winsock! " << wsOk<<endl;
 	}
+	
+	
+	SOCKET in = socket(AF_INET, SOCK_DGRAM, 0);
+	sockaddr_in serverHint;
+	serverHint.sin_addr.S_un.S_addr = ADDR_ANY; // Us any IP address available on the machine
+	serverHint.sin_family = AF_INET; // Address format is IPv4
+	serverHint.sin_port = htons(54000); 
+	if (bind(in, (sockaddr*)&serverHint, sizeof(serverHint)) == SOCKET_ERROR)
+	{
+		cout << "Can't bind socket! " << WSAGetLastError() << endl;
+		return;
+	}
+
+	sockaddr_in client; // Use to hold the client information (port / ip address)
+	int clientLength = sizeof(client); // The size of the client information
+
+	char buf[1024];
+
+	// Enter a loop
+	while (true)
+	{
+		ZeroMemory(&client, clientLength); // Clear the client structure
+		ZeroMemory(buf, 1024); // Clear the receive buffer
+
+		int bytesIn = recvfrom(in, buf, 1024, 0, (sockaddr*)&client, &clientLength);
+		if (bytesIn == SOCKET_ERROR)
+		{
+			cout << "Error receiving from client " << WSAGetLastError() << endl;
+			continue;
+		}
+		char clientIp[256]; 
+		ZeroMemory(clientIp, 256); 
+		inet_ntop(AF_INET, &client.sin_addr, clientIp, 256);
+
+		cout << "Message recv from " << clientIp << " : " << buf << endl;
+	}
+	closesocket(in);
+	WSACleanup();
+}
