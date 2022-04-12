eb17ec2b6c6420ac935157a4def303a633bb8729
AlbinDemukaj <94542503+AlbinDemukaj@users.noreply.github.com>
2022-04-12 23:28:22 +0200

    Update ConnectDissconnect.cpp

--- ConnectDissconnect.cpp
+++ ConnectDissconnect.cpp
@@ -58,6 +58,45 @@ void main()
 		cout << host << " connected on port " <<
 			ntohs(client.sin_port) << endl;
 	}
+	// Close listening socket
+	closesocket(listening);
+
+	// While loop: accept and echo message back to client
+	char buf[4096];
+
+	while (true)
+	{
+		ZeroMemory(buf, 4096);
+
+		// Wait for client to send data
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			cerr << "Error in recv(). Quitting" << endl;
+			break;
+		}
+
+		if (bytesReceived == 0)
+		{
+			cout << "Client disconnected " << endl;
+			break;
+		}
+
+		cout << string(buf, 0, bytesReceived) << endl;
+
+		// Echo message back to client
+		send(clientSocket, buf, bytesReceived + 1, 0);
+	
+	}
+	
+	// Close the socket
+	closesocket(clientSocket);
+
+	// Cleanup winsock
+	WSACleanup();
+
+	system("pause");
+}
 
 	
 
