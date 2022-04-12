22322b8cdc4802917205e19406ea68af87573f17
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2022-04-12 20:58:54 +0200

    Krijimi i socket ne serverin me multikliente

---
+++ Multiklientet.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+	// Initialze winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "Can't Initialize winsock! Quitting" << endl;
+		return;
+	}
+	
+	// Create a socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Can't create a socket! Quitting" << endl;
+		return;
+	}
