242e033f49d2409978bc080ef22b7d51e45f4888
AlbinDemukaj <94542503+AlbinDemukaj@users.noreply.github.com>
2022-04-12 23:16:12 +0200

    Create ConnectDissconnect.cpp

---
+++ ConnectDissconnect.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+	// Initialze winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "Can't Initialize winsock! Quitting" << endl;
+		return;
+	}
