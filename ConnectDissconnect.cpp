94740ff4068877b14a674d036fc430f04d514771
Albert Grajçevci <94078877+albertgrajqevci@users.noreply.github.com>
2022-04-12 23:24:56 +0200

    Update ConnectDissconnect.cpp

--- ConnectDissconnect.cpp
+++ ConnectDissconnect.cpp
@@ -18,3 +18,24 @@ void main()
 		cerr << "Can't Initialize winsock! Quitting" << endl;
 		return;
 	}
+	// Create a socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Can't create a socket! Quitting" << endl;
+		return;
+	}
+
+	// Bind the ip address and port to a socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton .... 
+	
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+	// Tell Winsock the socket is for listening 
+	listen(listening, SOMAXCONN);
+
+	
+
